e7dcf34c39bca8e38352aaf6cc77603c0dfdb803
okakaabel <okakaabel43@gmail.com>
2025-03-28 06:01:15 -0700

    we

--- database/database.js
+++ database/database.js
@@ -2,10 +2,10 @@
 
 const { Pool } = require('pg');  // PostgreSQL module
 const pool = new Pool({
-  user: 'your-username',   // Replace with actual DB username
-  host: 'localhost',       // Replace with actual DB host
-  database: 'your-database',  // Replace with actual database name
-  password: 'your-password',  // Replace with actual DB password
+  user: 'gab34',   // Replace with actual DB username
+  host: 'dpg-cvdvlhlumphs73bm2big-a.oregon-postgres.render',       // Replace with actual DB host
+  database: 'gab34',  // Replace with actual database name
+  password: 'eZ9rNiP0asiNDccVx2FGtFCaRVlfmihK',  // Replace with actual DB password
   port: 5432,  // Replace with actual DB port (5432 is default for PostgreSQL)
 });
 
