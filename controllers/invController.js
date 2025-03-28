35382cc3a4e9c92cc4f35e410121b424fdf8d4fb
okakaabel <okakaabel43@gmail.com>
2025-03-28 05:07:20 -0700

    qas

--- controllers/invController.js
+++ controllers/invController.js
@@ -1,44 +1,77 @@
-// Update for the vehicle detail view route
-async function vehicleDetailView(req, res, next) {
-  try {
-    const inv_id = parseInt(req.params.inv_id, 10);
-    const nav = await utilities.getNav();
+const invModel = require("../models/inventory-model")
+const utilities = require("../utilities/")
 
-    // Fetch vehicle data from model
-    const itemData = await invModel.getDetailByVehicleId(inv_id);
-    
-    if (!itemData) {
-      return next(new Error('Vehicle not found'));
+const invCont = {}
+
+/* ***************************
+ *  Build inventory by classification view
+ * ************************** */
+invCont.buildByClassificationId = async function (req, res, next) {
+  try {
+    const classification_id = req.params.classificationId;
+    const data = await invModel.getInventoryByClassificationId(classification_id);
+
+    if (!data || data.length === 0) {
+      const error = new Error("Vehicle classification not found");
+      error.status = 404;
+      throw error;
     }
 
-    // Format the price and mileage
-    itemData.inv_price = formatPrice(itemData.inv_price);
-    itemData.inv_miles = formatMileage(itemData.inv_miles);
-
-    // Use the utility function to build vehicle page HTML
-    const vehicleHTML = await utilities.buildVehiclePage(itemData);
-
-    res.render('inventory/detail', {
-      title: `${itemData.inv_make} ${itemData.inv_model} Details`,
+    const grid = await utilities.buildClassificationGrid(data);
+    const nav = await utilities.getNav();
+    const className = data[0].classification_name;
+    res.render("./inventory/classification", {
+      title: `${className} Vehicles`,
       nav,
-      vehicleHTML,
+      grid,
       errors: null,
     });
-  } catch (err) {
-    next(err);
+  } catch (error) {
+    next(error);
+  }
+};
+
+
+/* ***************************
+ *  Build vehicle page by inventory id
+ * ************************** */
+invCont.buildByInvId = async function (req, res, next) {
+  try {
+    const inv_id = req.params.invId;
+    const data = await invModel.getDetailByVehicleId(inv_id);
+
+    if (!data) {
+      const error = new Error("Vehicle not found");
+      error.status = 404;
+      throw error;
+    }
+
+    const vehicleTemplate = await utilities.buildVehiclePage(data);
+    const nav = await utilities.getNav();
+    const vehicleName = `${data.inv_year} ${data.inv_make} ${data.inv_model}`;
+    res.render("./inventory/vehicle", {
+      title: vehicleName,
+      nav,
+      vehicleTemplate,
+      errors: null,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+/* ***************************
+ *  Return Inventory by Classification As JSON
+ * ************************** */
+invCont.getInventoryJSON = async (req, res, next) => {
+  const classification_id = parseInt(req.params.classification_id)
+  const invData = await invModel.getInventoryByClassificationId(classification_id)
+  if (invData[0].inv_id) {
+    return res.json(invData)
+  } else {
+    next(new Error("No data returned"))
   }
 }
 
-// Format price to include commas and currency symbol
-function formatPrice(price) {
-  return "$" + new Intl.NumberFormat("en-US").format(price);
-}
 
-// Format mileage to include commas
-function formatMileage(mileage) {
-  return new Intl.NumberFormat("en-US").format(mileage) + " miles";
-}
-
-module.exports = {
-  vehicleDetailView,
-};
+module.exports = invCont
