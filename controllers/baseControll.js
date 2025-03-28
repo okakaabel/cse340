a40f4116924a82304399f695fc72e864f20b7ccd
okakaabel <okakaabel43@gmail.com>
2025-03-28 04:44:09 -0700

    weas

--- controllers/baseControll.js
+++ controllers/baseControll.js
@@ -1,13 +1,13 @@
-const utilities = require("../utilities/")
+const utilities = require("../utilities/"); // Make sure utilities.js is at the correct path
 const baseController = {}
 
-baseController.buildHome = async function(req, res){
-  const nav = await utilities.getNav()
-  // req.flash("notice", "This is a flash message.")
+baseController.buildHome = async function(req, res) {
+  const nav = await utilities.getNav() // Ensure utilities.getNav() exists and works fine
   res.render("index", {
     title: "Home",
     nav,
-    errors: null})
+    errors: null
+  });
 }
 
-module.exports = baseController
+module.exports = baseController;
