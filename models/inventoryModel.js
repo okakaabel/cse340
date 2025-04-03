f34dca77e1807d4d4497e884718ede2a63bdaede
okakaabel <okakaabel43@gmail.com>
2025-04-03 05:23:05 -0700

    htyj

--- models/inventoryModel.js
+++ models/inventoryModel.js
@@ -1,4 +1,4 @@
-const pool = require("../database/db.sql");
+const pool = require("../database/db");
 async function getVehicleById(id) {
     try {
         const result = await pool.query("SELECT * FROM inventory WHERE inventory_id = $1", [id]);
