0d4d85d3b3bb79803ad6e8b28e3c84fe3eec30bb
okakaabel <okakaabel43@gmail.com>
2025-04-10 21:35:54 +0300

    wasde

--- controllers/errorController.js
+++
@@ -1,4 +0,0 @@
-function triggerError(req, res, next) {
-    next(new Error("Intentional Server Error"));
-}
-module.exports = { triggerError };
--- public/js/inv-update.js
+++
@@ -1,5 +0,0 @@
-const form = document.querySelector("#updateForm")
-    form.addEventListener("change", function () {
-      const updateBtn = document.querySelector("button")
-      updateBtn.removeAttribute("disabled")
-    })
